537b4164b617433c9b5e5624c12b5bd823f66716
☺唐鳳☻ <cpan@audreyt.org>
2002-10-12 10:33:29 +0000

    Import of AUTRIJUS/Module-Signature-0.09 from CPAN.
    gitpan-cpan-distribution: Module-Signature
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         AUTRIJUS/Module-Signature-0.09.tar.gz
    gitpan-cpan-author:       AUTRIJUS
    gitpan-cpan-maturity:     released

--- Signature.pm
+++ Signature.pm
@@ -1,12 +1,12 @@
 # $File: //member/autrijus/Module-Signature/Signature.pm $ 
-# $Revision: #15 $ $Change: 1329 $ $DateTime: 2002/10/11 19:00:33 $
+# $Revision: #18 $ $Change: 1346 $ $DateTime: 2002/10/12 10:03:22 $
 
 package Module::Signature;
-$Module::Signature::VERSION = '0.08';
+$Module::Signature::VERSION = '0.09';
 
 use strict;
 use vars qw($VERSION $SIGNATURE @ISA @EXPORT_OK);
-use vars qw($BoilerPlate $Cipher $Debug $Quiet $KeyServer); 
+use vars qw($Preamble $Cipher $Debug $Quiet $KeyServer); 
 
 use constant SIGNATURE_OK        => 0;
 use constant SIGNATURE_MISSING   => -1;
@@ -14,19 +14,21 @@ use constant SIGNATURE_MALFORMED => -2;
 use constant SIGNATURE_BAD       => -3;
 use constant SIGNATURE_MISMATCH  => -4;
 use constant MANIFEST_MISMATCH   => -5;
+use constant CIPHER_UNKNOWN      => -6;
 
 use Digest;
 use ExtUtils::Manifest ();
 use Exporter;
 
 @EXPORT_OK	= (qw(sign verify),
+		   qw($SIGNATURE $KeyServer $Cipher $Preamble),
 		   grep /^[A-Z_]+_[A-Z_]+$/, keys %Module::Signature::);
 @ISA		= 'Exporter';
 
 $SIGNATURE	= 'SIGNATURE';
 $KeyServer	= 'pgp.mit.edu';
-$Cipher		= 'SHA1';
-$BoilerPlate	= << ".";
+$Cipher		= 'MD5';
+$Preamble	= << ".";
 This file contains message digests of all files listed in MANIFEST,
 signed via the Module::Signature module, version $VERSION.
 
@@ -48,7 +50,7 @@ Module::Signature - Module signature file manipulation
 
 =head1 VERSION
 
-This document describes version 0.08 of B<Module::Signature>.
+This document describes version 0.09 of B<Module::Signature>.
 
 =head1 SYNOPSIS
 
@@ -75,27 +77,71 @@ In programs:
     # see the CONSTANTS section below
     (verify() == SIGNATURE_OK) or die "failed!";
 
-CPAN authors may consider adding this code as C<t/0-signature.t>:
+CPAN authors may consider adding this code as F<t/0-signature.t>:
 
+    #!/usr/bin/perl
     use strict;
-    use Test::More tests => 1;
+    print "1..1\n";
 
-    SKIP: {
-	if (eval { require Module::Signature; 1 }) {
-	    ok(Module::Signature::verify() == Module::Signature::SIGNATURE_OK()
-		=> "Valid signature");
-	}
-	else {
-	    diag("Next time around, consider install Module::Signature,\n".
-		 "so you can verify the integrity of this distribution.\n");
-	    skip("Module::Signature not installed", 1)
-	}
+    if (eval { require Module::Signature; 1 }) {
+	(Module::Signature::verify() == Module::Signature::SIGNATURE_OK())
+	    or print "not ";
+	print "ok 1 # Valid signature\n";
     }
+    else {
+	warn "# Next time around, consider install Module::Signature,\n".
+             "# so you can verify the integrity of this distribution.\n";
+	print "ok 1 # skip - Module::Signature not installed\n";
+    }
+
+If you are already using B<Test::More> for testing, a more
+straightforward version of F<t/0-signature.t> can be found in the
+B<Module::Signature> distribution.
+
+And if you're not worried about compatibility of Perl 5.005 and earlier
+versions, willing to inflict the dependency of B<Module::Build> on your
+users, and prefer a more full-fledged testing package, Iain Truskett's
+B<Test::Signature> might be a better choice.
 
 =head1 DESCRIPTION
 
-B<Module::Signature> adds cryptographic authentications to
-CPAN distribution files, via the special SIGNATURE file.
+B<Module::Signature> adds cryptographic authentications to CPAN
+distributions, via the special SIGNATURE file.
+
+=head1 VARIABLES
+
+These package variables are not exported by default.
+
+=over 4
+
+=item $SIGNATURE
+
+The filename for a distribution's signature file.  Defaults to
+C<SIGNATURE>.
+
+=item $KeyServer
+
+The OpenPGP key server for fetching the author's public key
+(currently only implemented on C<gpg>, not C<Crypt::OpenPGP>).
+May be set to a false value to prevent this module from
+fetching public keys.
+
+=item $Cipher
+
+The default cipher used by the C<Digest> module to make signature
+files.  Defaults to C<SHA1>, but may be changed to other ciphers
+if the SHA1 cipher is undesirable for the user.
+
+Module::Signature version 0.09 and above will use the cipher
+specified in the SIGNATURE file's first entry to validate its
+integrity.
+
+=item $Preamble
+
+The explanatory text written to newly generated SIGNATURE files
+before the actual entries.
+
+=back
 
 =head1 CONSTANTS
 
@@ -125,10 +171,16 @@ since its creation.
 There are extra files in the current directory not specified by
 the MANIFEST file.
 
+=item CIPHER_UNKNOWN
+
+The cipher used by the signature file is not recognized by the
+C<Digest> module.
+
+=back
+
 =cut
 
 sub verify {
-    my $plaintext = _mkdigest();
     my $rv;
 
     (-r $SIGNATURE) or do {
@@ -139,6 +191,16 @@ sub verify {
     (my $sigtext = _read_sigfile($SIGNATURE)) or do {
 	warn "==> MALFORMED Signature file! <==\n";
 	return SIGNATURE_MALFORMED;
+    };
+
+    (my ($cipher) = ($sigtext =~ /^(\w+) /)) or do {
+	warn "==> MALFORMED Signature file! <==\n";
+	return SIGNATURE_MALFORMED;
+    };
+
+    (defined(my $plaintext = _mkdigest($cipher))) or do {
+	warn "==> UNKNOWN Cipher format! <==\n";
+	return CIPHER_UNKNOWN;
     };
 
     if (`gpg --version` =~ /GnuPG/) {
@@ -282,7 +344,7 @@ sub _sign_gpg {
 
     local *D;
     open D, ">$sigfile" or die "Could not write to $sigfile: $!";
-    print D $BoilerPlate;
+    print D $Preamble;
     close D;
     open D, "| gpg --clearsign >> $sigfile" or die "Could not call gpg: $!";
     print D $plaintext;
@@ -316,7 +378,7 @@ sub _sign_crypt_openpgp {
 
     local *D;
     open D, ">$sigfile" or die "Could not write to $sigfile: $!";
-    print D $BoilerPlate;
+    print D $Preamble;
     print D $signature;
     close D;
 
@@ -324,7 +386,7 @@ sub _sign_crypt_openpgp {
 }
 
 sub _mkdigest {
-    my $digest = _mkdigest_files(@_);
+    my $digest = _mkdigest_files(undef, @_) or return;
     my $plaintext = '';
 
     foreach my $file (sort keys %$digest) {
@@ -342,7 +404,9 @@ sub _mkdigest_files {
     my $read = ExtUtils::Manifest::maniread() || {};
     my $found = ExtUtils::Manifest::manifind($p);
     my(%digest) = ();
-    my $obj = Digest->new($algorithm);
+    my $obj = eval { Digest->new($algorithm) } or do {
+	warn("Unknown cipher: $algorithm\n"); return;
+    };
 
     foreach my $file (sort keys %$read){
         warn "Debug: collecting digest from $file\n" if $Debug;
@@ -372,7 +436,7 @@ __END__
 
 =head1 SEE ALSO
 
-L<ExtUtils::Manifest>, L<Crypt::OpenPGP>
+L<ExtUtils::Manifest>, L<Crypt::OpenPGP>, L<Test::Signature>
 
 =head1 AUTHORS
 
